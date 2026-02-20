24db44203cd194f8896235434e77004cfbde7cb3
Kelvin B. <kelvinbhual@gmail.com>
2026-02-19 23:01:16 -0500

    Update pa01.c

--- pa01.c
+++ pa01.c
@@ -3,35 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 
-/*============================================================================
-| Assignment: pa01 - Encrypting a plaintext file using the Hill cipher
-|
-| Author: Kelvin Bhual
-| Language: c, c++, Java, go, python, rust
-| To Compile: javac pa01.java
-| gcc -o pa01 pa01.c
-| g++ -o pa01 pa01.cpp
-| go build pa01.go
-| rustc pa01.rs
-| To Execute: java -> java pa01 kX.txt pX.txt
-| or c++ -> ./pa01 kX.txt pX.txt
-| or c -> ./pa01 kX.txt pX.txt
-| or go -> ./pa01 kX.txt pX.txt
-| or rust -> ./pa01 kX.txt pX.txt
-| or python -> python3 pa01.py kX.txt pX.txt
-| where kX.txt is the keytext file
-| and pX.txt is plaintext file
-| Note:
-| All input files are simple 8 bit ASCII input
-| All execute commands above have been tested on Eustis
-|
-| Class: CIS3360 - Security in Computing - Spring 2025
-| Instructor: McAlpin
-| Due Date: 2/23/2025 at 11:59pm
-+===========================================================================*/
-
-
-
 int main(int argc, char **argv)
 {
   int i, j, k;
@@ -173,14 +144,3 @@ int main(int argc, char **argv)
   
   return 0;
 }
-
-
-
-/*=============================================================================
-| I Kelvin Bhual (ke203968) affirm that this program is
-| entirely my own work and that I have neither developed my code together with
-| any another person, nor copied any code from any other person, nor permitted
-| my code to be copied or otherwise used by any other person, nor have I
-| copied, modified, or otherwise used programs created by others. I acknowledge
-| that any violation of the above terms will be treated as academic dishonesty.
-+=============================================================================*/
